61567762cc377f847688f2e935ccc401378f5fb4
ukim0922 <ukim0922@hanmail.net>
2016-05-21 22:23:58 +0900

    [기능번호:1] 진행중 (비트맵 생성)
    김유진

--- KKL_LOGISIM/KKL_LOGISIM/ChildView.cpp
+++ KKL_LOGISIM/KKL_LOGISIM/ChildView.cpp
@@ -26,6 +26,8 @@ CChildView::~CChildView()
 
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
 	ON_WM_PAINT()
+	ON_WM_LBUTTONUP()
+	ON_WM_LBUTTONDOWN()
 END_MESSAGE_MAP()
 
 
@@ -48,13 +50,39 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 void CChildView::OnPaint() 
 {
 	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
-	if (게이트 추가되면)
+	CBitmap andGate;
+	andGate.LoadBitmap(IDB_BITMAP_AND);
+	BITMAP andGateinfo;
+	andGate.GetBitmap(&andGateinfo);
+
+	if (/*게이트 추가되면*/1)
 	{
 		gate_result[gate_num - 1] = 'F';  //게이트 생성번호 == 출력번호 하고싶은데 string이 왜안써질까
 	}
 
-	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
-	dc.TextOutW(100, 100, _T("수정"));
+	
 	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
 }
 
+
+
+void CChildView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	// 유진 : 여기에 게이트에 마우스 댔을 때 초록동그라미 뜨도록 하기
+	CWnd::OnLButtonUp(nFlags, point);
+}
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	CClientDC dc(this);
+
+	twoInputoneOutput();
+	CWnd::OnLButtonDown(nFlags, point);
+}
+
+void CChildView::twoInputoneOutput() {
+	//두입력한출력 인식하도록?
+}
--- KKL_LOGISIM/KKL_LOGISIM/ChildView.h
+++ KKL_LOGISIM/KKL_LOGISIM/ChildView.h
@@ -32,5 +32,9 @@ public:
 protected:
 	afx_msg void OnPaint();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	void twoInputoneOutput();//À¯Áø
 };
 
