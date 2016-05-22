48ae539630d9361f016ee7d88def0fd32560bd53
김유진 <ukim0922@hanmail.net>
2016-05-22 22:05:14 +0900

    프로젝트 생성

---
+++ Logisim_KKL/Logisim_KKL/Logisim_KKLView.cpp
@@ -0,0 +1,81 @@
+
+// Logisim_KKLView.cpp : CLogisim_KKLView 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Logisim_KKL.h"
+#endif
+
+#include "Logisim_KKLDoc.h"
+#include "Logisim_KKLView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CLogisim_KKLView
+
+IMPLEMENT_DYNCREATE(CLogisim_KKLView, CView)
+
+BEGIN_MESSAGE_MAP(CLogisim_KKLView, CView)
+END_MESSAGE_MAP()
+
+// CLogisim_KKLView 생성/소멸
+
+CLogisim_KKLView::CLogisim_KKLView()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CLogisim_KKLView::~CLogisim_KKLView()
+{
+}
+
+BOOL CLogisim_KKLView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CLogisim_KKLView 그리기
+
+void CLogisim_KKLView::OnDraw(CDC* /*pDC*/)
+{
+	CLogisim_KKLDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CLogisim_KKLView 진단
+
+#ifdef _DEBUG
+void CLogisim_KKLView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CLogisim_KKLView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CLogisim_KKLDoc* CLogisim_KKLView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CLogisim_KKLDoc)));
+	return (CLogisim_KKLDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CLogisim_KKLView 메시지 처리기
