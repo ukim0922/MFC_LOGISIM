807e4b67f70627f58db2447b114a403c1d941ff8
김서영 <kseoy4046@naver.com>
2016-05-28 15:07:21 +0900

    [기능4,5,6번] NAND, NOR, XOR 논리 구현 및 비트맵 추가

--- Logisim_KKL/Logisim_KKL/LogicEngine.cpp
+++ Logisim_KKL/Logisim_KKL/LogicEngine.cpp
@@ -21,10 +21,22 @@ void LogicEngine::Gate(GateSelect Select, bool & Input1, bool & Input2)
 		LOR(Input1, Input2);
 		break;
 	case NAND:
+		if (Input1 && Input2)
+			Output == FALSE;
+		else
+			Output == TRUE;
 		break;
 	case NOR:
+		if (Input1 || Input2)
+			Output == FALSE;
+		else
+			Output == TRUE;
 		break;
 	case XOR:
+		if (Input1 == Input2)
+			Output == FALSE;
+		else
+			Output == TRUE;
 		break;
 	default:
 		AfxMessageBox(_T("Overloding ERROR"), MB_OKCANCEL); //NOT gate ÀÎ °æ¿ì
--- Logisim_KKL/Logisim_KKL/Resource.h
+++ Logisim_KKL/Logisim_KKL/Resource.h
@@ -8,12 +8,15 @@
 #define IDB_BITMAP1                     310
 #define IDB_BITMAP2                     311
 #define IDB_BITMAP_NOT                  312
+#define IDB_BITMAP_NAND                 313
+#define IDB_BITMAP_NOR                  314
+#define IDB_BITMAP_XOR                  315
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_RESOURCE_VALUE        316
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
