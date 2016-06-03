ca280e1c94f0b5e8c05ded5e5c893e7abcce0361
이정민 <aabbccc9613@naver.com>
2016-06-04 05:28:56 +0900

    동기화

--- Logisim_KKL/Logisim_KKL/Line.cpp
+++ Logisim_KKL/Logisim_KKL/Line.cpp
@@ -1,7 +1,29 @@
 #include "stdafx.h"
 #include "Line.h"
 
+Line::Line(int x, int y)
+{
+	this->line[0] = CPoint(x, y);
+	this->line[1] = CPoint(x, y);
+}
+
+Line::Line(CPoint point)
+{
+	this->line[0] = point;
+	this->line[1] = point;
+	
+}
+
 
 Line::~Line()
 {
+}
+
+
+void draw_main(Gdiplus::Graphics* gp)
+{
+	Gdiplus::Pen *p;
+	p = new Gdiplus::Pen(Gdiplus::Color(0, 0, 0), 3);
+	Gdiplus::Point drw_line[2];
+	gp->DrawLines(p, drw_line, 2);
 }
--- Logisim_KKL/Logisim_KKL/Line.h
+++ Logisim_KKL/Logisim_KKL/Line.h
@@ -1,10 +1,17 @@
 #pragma once
+#include "stdafx.h"
+#include <vector>
+
 class Line
 {
 public:
-	int l_num = 0;
-	bool statu;
-	Line() { statu = false; }
+	CPoint line[2];
+	bool statu = false;
+	std::vector<Line *> connect_lines;
+	void draw_main(Gdiplus::Graphics* gp);
+	Line(int x, int y);
+	Line(CPoint point);
+	Line();
 	~Line();
 };
 
